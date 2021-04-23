d171f2c332733affacf4c4ed7372de38e703df8e
hiroto-onda <1610r.o.d@gmail.com>
2021-04-23 18:09:44 +0900

    ユーザー管理機能修正２

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -77,6 +77,52 @@ RSpec.describe User, type: :model do
           @user.valid?
           expect(@user.errors.full_messages).to include("Date can't be blank")
         end
+        it "family_nameは半角だと登録できない" do
+          @user.family_name = "a"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Family name is invalid")
+        end
+        it "first_nameは半角だと登録できない" do
+          @user.first_name = "a"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("First name is invalid")
+        end
+        it "family_name_kanaは半角だとと登録できない" do
+          @user.family_name_kana = "a"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Family name kana is invalid")
+        end
+        it "family_name_kanaは全角かつカタカナでないと登録できない" do
+          @user.family_name_kana = "a"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Family name kana is invalid")
+        end
+        it "first_name_kanaは半角だと登録できない
+        " do
+          @user.first_name_kana = "a"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("First name kana is invalid")
+        end
+        it "first_name_kanaは全角かつカタカナでないと登録できない" do
+          @user.first_name_kana = "a"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("First name kana is invalid")
+        end
+        it "passwordは半角英語のみだと登録できない" do
+          @user.password = "aaaaaa"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password is invalid")
+        end
+        it "passwordは半角数字のみだと登録できない" do
+          @user.first_name_kana = "111111"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("First name kana is invalid")
+        end
+        it "passwordは全角英数混合だと登録できない" do
+          @user.first_name_kana = "AAA１１１"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("First name kana is invalid")
+        end
       end
     end
   end
